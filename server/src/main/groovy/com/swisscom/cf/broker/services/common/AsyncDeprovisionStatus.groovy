7cbd1ba385691ed87bf4151749b370045795c3da
Murat Celep <murat.celep@swisscom.com>
2016-12-19 15:13:31 +0100

    remove unused class

--- server/src/main/groovy/com/swisscom/cf/broker/services/common/AsyncDeprovisionStatus.groovy
+++
@@ -1,8 +0,0 @@
-package com.swisscom.cf.broker.services.common
-
-import com.swisscom.cf.broker.provisioning.async.AsyncOperationStatus
-import groovy.transform.CompileStatic
-
-@CompileStatic
-class AsyncDeprovisionStatus implements AsyncOperationStatus {
-}
