a2f95a633962d6548e848dca50f1ffbf52983d74
Philipp Meier <philipp.meier2@swisscom.com>
2017-07-11 13:18:36 +0200

    Fix Gradle task to upload Snapshots for Broker module

--- travis-publish.sh
+++ travis-publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'develop' ]; then
-  ./gradlew -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}" build uploadArchives -x functionalTest
+  ./gradlew uploadArchives -x functionalTest -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}"
 elif [ ${TRAVIS_PULL_REQUEST} = 'false' ] && [ ${TRAVIS_BRANCH} = 'master' ]; then
-  ./gradlew -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}" -PsigningKeyId="${SIGNING_KEY_ID}" -PsigningPassword="${SIGNING_PASSWORD}" -PsigningSecretKeyRingFile="$(pwd)/local.secring.gpg" uploadArchives closeAndReleaseRepository -x functionalTest
+  ./gradlew uploadArchives closeAndReleaseRepository -x functionalTest -PossrhUsername="${SONATYPE_USERNAME}" -PossrhPassword="${SONATYPE_PASSWORD}" -PsigningKeyId="${SIGNING_KEY_ID}" -PsigningPassword="${SIGNING_PASSWORD}" -PsigningSecretKeyRingFile="$(pwd)/local.secring.gpg"
 fi
