7de825ec2836906b5d6355146f570293a8fd5b44
Murat Celep <murat.celep@swisscom.com>
2017-07-14 14:16:50 +0200

    allow external configuration to take precedence for overriding configuration values

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/ServiceBroker.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/ServiceBroker.groovy
@@ -1,7 +1,8 @@
 package com.swisscom.cloud.sb.broker
 
+import com.swisscom.cloud.sb.broker.config.StandAloneConfigurationInitializer
 import com.swisscom.cloud.sb.broker.config.WebContainerConfigurationInitializer
-import org.springframework.boot.SpringApplication
+import groovy.transform.CompileStatic
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
@@ -10,9 +11,11 @@ import org.springframework.boot.web.support.SpringBootServletInitializer
 
 @SpringBootApplication(exclude = [org.springframework.cloud.servicebroker.config.ServiceBrokerAutoConfiguration.class,
         MongoAutoConfiguration.class, MongoDataAutoConfiguration.class])
+@CompileStatic
 class ServiceBroker extends SpringBootServletInitializer {
 	static void main(String[] args) {
-        SpringApplication.run(ServiceBroker.class, args)
+		new SpringApplicationBuilder(ServiceBroker.class)
+				.initializers(new StandAloneConfigurationInitializer()).run(args)
 	}
 
 	@Override
