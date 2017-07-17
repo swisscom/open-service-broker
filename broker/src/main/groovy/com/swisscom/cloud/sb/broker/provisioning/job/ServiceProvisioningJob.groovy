603db1be814777dd682e661e571d83ebc3ad349a
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-07-17 11:05:48 +0200

    fix configuration of integers

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/provisioning/job/ServiceProvisioningJob.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/provisioning/job/ServiceProvisioningJob.groovy
@@ -40,7 +40,7 @@ class ServiceProvisioningJob extends AbstractLastOperationJob {
     protected AsyncOperationResult handleJob(LastOperationJobContext context) {
         log.info("About to request service provision, ${context.lastOperation.toString()}")
         AsyncOperationResult provisionResult = findServiceProvisioner(context).requestProvision(context)
-        provisioningPersistenceService.createServiceInstanceOrUpdateDetails(context.provisionRequest, new ProvisionResponse(details: provisionResult.details, isAsync: true))
+        context.serviceInstance = provisioningPersistenceService.createServiceInstanceOrUpdateDetails(context.provisionRequest, new ProvisionResponse(details: provisionResult.details, isAsync: true))
         if (provisionResult.status == LastOperation.Status.SUCCESS) {
             provisioningPersistenceService.updateServiceInstanceCompletion(context.serviceInstance, true)
         }
