2d8b6395bba01d39e02ccc8040aa155a0a69f681
Konrad Lykowski <konrad.lykowski@outlook.com>
2017-07-31 17:33:04 +0200

    clean duplicate code for async job

--- broker/src/integration-test/groovy/com/swisscom/cloud/sb/broker/util/DummyConfig.groovy
+++ broker/src/integration-test/groovy/com/swisscom/cloud/sb/broker/util/DummyConfig.groovy
@@ -1,9 +1,10 @@
 package com.swisscom.cloud.sb.broker.util
 
+import com.swisscom.cloud.sb.broker.services.AsyncServiceConfig
 import com.swisscom.cloud.sb.broker.services.bosh.BoshBasedServiceConfig
 import groovy.transform.CompileStatic
 
 
 @CompileStatic
-class DummyConfig implements BoshBasedServiceConfig {
+class DummyConfig implements BoshBasedServiceConfig, AsyncServiceConfig {
 }
---
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/AsyncServiceConfig.groovy
@@ -0,0 +1,11 @@
+package com.swisscom.cloud.sb.broker.services
+
+import com.swisscom.cloud.sb.broker.cfextensions.endpoint.EndpointConfig
+import com.swisscom.cloud.sb.broker.services.bosh.BoshConfig
+import groovy.transform.CompileStatic
+
+@CompileStatic
+trait AsyncServiceConfig implements EndpointConfig, BoshConfig {
+    int retryIntervalInSeconds
+    int maxRetryDurationInMinutes
+}
