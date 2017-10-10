696781131b5ad35f271f2ef968877d3bf7f95016
Philipp Meier <philipp.meier2@swisscom.com>
2017-10-10 07:43:27 +0200

    Refactor AbstractKubernetesFacade to more common abtract methods `provision` and `getServiceBinding`

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/facade/AbstractKubernetesFacade.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/facade/AbstractKubernetesFacade.groovy
@@ -1,15 +1,23 @@
 package com.swisscom.cloud.sb.broker.services.kubernetes.facade
 
 import com.swisscom.cloud.sb.broker.model.DeprovisionRequest
+import com.swisscom.cloud.sb.broker.model.Plan
+import com.swisscom.cloud.sb.broker.model.ProvisionRequest
+import com.swisscom.cloud.sb.broker.model.ServiceDetail
 import com.swisscom.cloud.sb.broker.services.kubernetes.client.rest.KubernetesClient
 import com.swisscom.cloud.sb.broker.services.kubernetes.config.AbstractKubernetesServiceConfig
 import com.swisscom.cloud.sb.broker.services.kubernetes.config.KubernetesConfig
 import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.EndpointMapper
+import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.parameters.EndpointMapperParamsDecorated
+import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.parameters.KubernetesConfigUrlParams
 import com.swisscom.cloud.sb.broker.services.kubernetes.templates.KubernetesTemplate
+import com.swisscom.cloud.sb.broker.services.kubernetes.templates.KubernetesTemplateManager
+import com.swisscom.cloud.sb.broker.services.kubernetes.templates.constants.BaseTemplateConstants
 import groovy.json.JsonSlurper
 import groovy.transform.TypeChecked
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.util.Pair
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,16 +26,67 @@ import org.springframework.web.client.HttpStatusCodeException
 import static groovy.transform.TypeCheckingMode.SKIP
 
 @Slf4j
-abstract class AbstractKubernetesFacade implements KubernetesFacade {
+abstract class AbstractKubernetesFacade<T extends AbstractKubernetesServiceConfig> implements KubernetesFacade {
     protected final KubernetesClient<?> kubernetesClient
     protected final KubernetesConfig kubernetesConfig
-    protected final AbstractKubernetesServiceConfig kubernetesServiceConfig
+    protected final KubernetesTemplateManager kubernetesTemplateManager
+    protected final EndpointMapperParamsDecorated endpointMapperParamsDecorated
+    protected final T kubernetesServiceConfig
 
     @Autowired
-    AbstractKubernetesFacade(KubernetesClient kubernetesClient, KubernetesConfig kubernetesConfig, AbstractKubernetesServiceConfig abstractKubernetesServiceConfig) {
+    AbstractKubernetesFacade(KubernetesClient kubernetesClient, KubernetesConfig kubernetesConfig, KubernetesTemplateManager kubernetesTemplateManager, EndpointMapperParamsDecorated endpointMapperParamsDecorated, AbstractKubernetesServiceConfig abstractKubernetesServiceConfig) {
         this.kubernetesClient = kubernetesClient
         this.kubernetesConfig = kubernetesConfig
+        this.kubernetesTemplateManager = kubernetesTemplateManager
+        this.endpointMapperParamsDecorated = endpointMapperParamsDecorated
         this.kubernetesServiceConfig = abstractKubernetesServiceConfig
+    }
+
+    protected abstract Map<String, String> getBindingMap(ProvisionRequest context)
+
+    protected
+    abstract Collection<ServiceDetail> buildServiceDetailsList(Map<String, String> bindingMap, List<ResponseEntity> responses)
+
+    Collection<ServiceDetail> provision(ProvisionRequest context) {
+        def bindingMap = getBindingMap(context)
+        log.debug("Use this bindings for k8s templates: ${groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(bindingMap))}")
+        def templates = kubernetesTemplateManager.getTemplates(context.plan.templateUniqueIdentifier)
+        def templateEngine = new groovy.text.SimpleTemplateEngine()
+        List<ResponseEntity> responses = new LinkedList()
+        for (KubernetesTemplate kubernetesTemplate : templates) {
+            def bindedTemplate = templateEngine.createTemplate(fixTemplateEscaping(kubernetesTemplate)).make(bindingMap).toString()
+            log.trace("Request this template for k8s provision: ${bindedTemplate}")
+            Pair<String, ?> urlReturn = endpointMapperParamsDecorated.getEndpointUrlByTypeWithParams(KubernetesTemplate.getKindForTemplate(bindedTemplate), (new KubernetesConfigUrlParams()).getParameters(context))
+            responses.add(kubernetesClient.exchange(urlReturn.getFirst(), HttpMethod.POST, bindedTemplate, urlReturn.getSecond().class))
+        }
+        return buildServiceDetailsList(bindingMap, responses)
+    }
+
+    protected Map<String, String> getServiceDetailBindingMap(ProvisionRequest context) {
+        [
+                (BaseTemplateConstants.SERVICE_ID.getValue()): context.getServiceInstanceGuid(),
+                (BaseTemplateConstants.SPACE_ID.getValue())  : context.getSpaceGuid(),
+                (BaseTemplateConstants.ORG_ID.getValue())    : context.getOrganizationGuid(),
+                (BaseTemplateConstants.PLAN_ID.getValue())   : context.plan.guid,
+        ]
+    }
+
+    protected Map<String, String> getPlanParameterBindingMap(Plan plan) {
+        plan.parameters.collectEntries {
+            [(it.getName() as String): it.getValue() as String]
+        }
+    }
+
+    void deprovision(DeprovisionRequest request) {
+        try {
+            kubernetesClient.exchange(EndpointMapper.INSTANCE.getEndpointUrlByType("Namespace").getFirst() + "/" + request.serviceInstanceGuid,
+                    HttpMethod.DELETE, "", Object.class)
+        } catch (HttpStatusCodeException e) {
+            if (e.statusCode != HttpStatus.NOT_FOUND) throw e
+            else {
+                log.debug("404: Tried to delete namespace " + request.serviceInstanceGuid + " which was not found. Error Message:" + e.toString())
+            }
+        }
     }
 
     @Override
@@ -73,18 +132,6 @@ abstract class AbstractKubernetesFacade implements KubernetesFacade {
         def podList = jsonSlurper.parseText(podListResponse.body)
         assert podList.kind == 'PodList'
         return podList.items
-    }
-
-    void deprovision(DeprovisionRequest request) {
-        try {
-            kubernetesClient.exchange(EndpointMapper.INSTANCE.getEndpointUrlByType("Namespace").getFirst() + "/" + request.serviceInstanceGuid,
-                    HttpMethod.DELETE, "", Object.class)
-        } catch (HttpStatusCodeException e) {
-            if (e.statusCode != HttpStatus.NOT_FOUND) throw e
-            else {
-                log.debug("404: Tried to delete namespace " + request.serviceInstanceGuid + " which was not found. Error Message:" + e.toString())
-            }
-        }
     }
 
     /**
--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/facade/redis/KubernetesFacadeRedis.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/facade/redis/KubernetesFacadeRedis.groovy
@@ -2,7 +2,6 @@ package com.swisscom.cloud.sb.broker.services.kubernetes.facade.redis
 
 import com.swisscom.cloud.sb.broker.backup.SystemBackupProvider
 import com.swisscom.cloud.sb.broker.backup.shield.ShieldTarget
-import com.swisscom.cloud.sb.broker.model.DeprovisionRequest
 import com.swisscom.cloud.sb.broker.model.ProvisionRequest
 import com.swisscom.cloud.sb.broker.model.ServiceDetail
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
@@ -10,69 +9,33 @@ import com.swisscom.cloud.sb.broker.services.kubernetes.client.rest.KubernetesCl
 import com.swisscom.cloud.sb.broker.services.kubernetes.config.KubernetesConfig
 import com.swisscom.cloud.sb.broker.services.kubernetes.dto.Port
 import com.swisscom.cloud.sb.broker.services.kubernetes.dto.ServiceResponse
-import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.EndpointMapper
 import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.parameters.EndpointMapperParamsDecorated
-import com.swisscom.cloud.sb.broker.services.kubernetes.endpoint.parameters.KubernetesConfigUrlParams
 import com.swisscom.cloud.sb.broker.services.kubernetes.facade.AbstractKubernetesFacade
 import com.swisscom.cloud.sb.broker.services.kubernetes.facade.redis.config.KubernetesRedisConfig
-import com.swisscom.cloud.sb.broker.services.kubernetes.templates.KubernetesTemplate
 import com.swisscom.cloud.sb.broker.services.kubernetes.templates.KubernetesTemplateManager
-import com.swisscom.cloud.sb.broker.services.kubernetes.templates.constants.BaseTemplateConstants
 import com.swisscom.cloud.sb.broker.util.StringGenerator
 import com.swisscom.cloud.sb.broker.util.servicedetail.ServiceDetailsHelper
 import com.swisscom.cloud.sb.broker.util.servicedetail.ShieldServiceDetailKey
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.util.Pair
-import org.springframework.http.HttpMethod
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
-import org.springframework.web.client.HttpStatusCodeException
 
 @Component
 @Slf4j
 @CompileStatic
-class KubernetesFacadeRedis extends AbstractKubernetesFacade implements SystemBackupProvider {
-    private final KubernetesTemplateManager kubernetesTemplateManager
-    private final EndpointMapperParamsDecorated endpointMapperParamsDecorated
-    private final KubernetesRedisConfig kubernetesRedisConfig
+class KubernetesFacadeRedis extends AbstractKubernetesFacade<KubernetesRedisConfig> implements SystemBackupProvider {
 
     @Autowired
-    KubernetesFacadeRedis(KubernetesClient kubernetesClient, KubernetesConfig kubernetesConfig, KubernetesRedisConfig kubernetesRedisConfig, KubernetesTemplateManager kubernetesTemplateManager, EndpointMapperParamsDecorated endpointMapperParamsDecorated) {
-        super(kubernetesClient, kubernetesConfig, kubernetesRedisConfig)
-        this.kubernetesTemplateManager = kubernetesTemplateManager
-        this.endpointMapperParamsDecorated = endpointMapperParamsDecorated
-        this.kubernetesRedisConfig = kubernetesRedisConfig
+    KubernetesFacadeRedis(KubernetesClient kubernetesClient, KubernetesConfig kubernetesConfig, KubernetesTemplateManager kubernetesTemplateManager, EndpointMapperParamsDecorated endpointMapperParamsDecorated, KubernetesRedisConfig kubernetesRedisConfig) {
+        super(kubernetesClient, kubernetesConfig, kubernetesTemplateManager, endpointMapperParamsDecorated, kubernetesRedisConfig)
     }
 
-
-    Collection<ServiceDetail> provision(ProvisionRequest context) {
-        def bindingMap = createBindingMap(context)
-        log.debug("Use this bindings for k8s templates: ${groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(bindingMap))}")
-        def templates = kubernetesTemplateManager.getTemplates(context.plan.templateUniqueIdentifier)
-        def templateEngine = new groovy.text.SimpleTemplateEngine()
-        List<ResponseEntity> responses = new LinkedList()
-        for (KubernetesTemplate kubernetesTemplate : templates) {
-            def bindedTemplate = templateEngine.createTemplate(fixTemplateEscaping(kubernetesTemplate)).make(bindingMap).toString()
-            log.trace("Request this template for k8s provision: ${bindedTemplate}")
-            Pair<String, ?> urlReturn = endpointMapperParamsDecorated.getEndpointUrlByTypeWithParams(KubernetesTemplate.getKindForTemplate(bindedTemplate), (new KubernetesConfigUrlParams()).getParameters(context))
-            responses.add(kubernetesClient.exchange(urlReturn.getFirst(), HttpMethod.POST, bindedTemplate, urlReturn.getSecond().class))
-        }
-        return buildServiceDetailsList(bindingMap.get(KubernetesRedisTemplateConstants.REDIS_PASS.getValue()), responses)
-    }
-
-    private Map<String, String> createBindingMap(ProvisionRequest context) {
-        def serviceDetailBindings = [
-                (BaseTemplateConstants.SERVICE_ID.getValue()): context.getServiceInstanceGuid(),
-                (BaseTemplateConstants.SPACE_ID.getValue())  : context.getSpaceGuid(),
-                (BaseTemplateConstants.ORG_ID.getValue())    : context.getOrganizationGuid(),
-                (BaseTemplateConstants.PLAN_ID.getValue())   : context.plan.guid,
-        ]
-        Map<String, String> planBindings = context.plan.parameters.collectEntries {
-            [(it.getName() as String): it.getValue() as String]
-        }
+    @Override
+    protected Map<String, String> getBindingMap(ProvisionRequest context) {
+        def serviceDetailBindings = getServiceDetailBindingMap(context)
+        def planBindings = getPlanParameterBindingMap(context.plan)
         def redisPassword = new StringGenerator().randomAlphaNumeric(30)
         def slaveofCommand = new StringGenerator().randomAlphaNumeric(30)
         def configCommand = new StringGenerator().randomAlphaNumeric(30)
@@ -81,10 +44,11 @@ class KubernetesFacadeRedis extends AbstractKubernetesFacade implements SystemBa
                 (KubernetesRedisTemplateConstants.SLAVEOF_COMMAND.getValue()): slaveofCommand,
                 (KubernetesRedisTemplateConstants.CONFIG_COMMAND.getValue()) : configCommand
         ]
-        kubernetesRedisConfig.redisConfigurationDefaults << planBindings << serviceDetailBindings << otherBindings
+        kubernetesServiceConfig.redisConfigurationDefaults << planBindings << serviceDetailBindings << otherBindings
     }
 
-    private Collection<ServiceDetail> buildServiceDetailsList(String redisPassword, List<ResponseEntity> responses) {
+    @Override
+    protected Collection<ServiceDetail> buildServiceDetailsList(Map<String, String> bindingMap, List<ResponseEntity> responses) {
         def serviceResponses = responses.findAll { it?.getBody() instanceof ServiceResponse }.collect {
             it.getBody().asType(ServiceResponse.class)
         }
@@ -100,8 +64,8 @@ class KubernetesFacadeRedis extends AbstractKubernetesFacade implements SystemBa
             ServiceDetail.from(ShieldServiceDetailKey.SHIELD_AGENT_PORT, it.nodePort.toString())
         }
 
-        def serviceDetails = [ServiceDetail.from(KubernetesRedisServiceDetailKey.KUBERNETES_REDIS_PASSWORD, redisPassword),
-                              ServiceDetail.from(KubernetesRedisServiceDetailKey.KUBERNETES_REDIS_HOST, kubernetesRedisConfig.getKubernetesRedisHost())] +
+        def serviceDetails = [ServiceDetail.from(KubernetesRedisServiceDetailKey.KUBERNETES_REDIS_HOST, kubernetesServiceConfig.getKubernetesRedisHost()),
+                              ServiceDetail.from(KubernetesRedisServiceDetailKey.KUBERNETES_REDIS_PASSWORD, bindingMap.get(KubernetesRedisTemplateConstants.REDIS_PASS.getValue()))] +
                 masterPort + slavePorts + shieldPort
         return serviceDetails
     }
@@ -124,6 +88,6 @@ class KubernetesFacadeRedis extends AbstractKubernetesFacade implements SystemBa
 
     @Override
     String shieldAgentUrl(ServiceInstance serviceInstance) {
-        "${kubernetesRedisConfig.getKubernetesRedisHost()}:${ServiceDetailsHelper.from(serviceInstance.details).getValue(ShieldServiceDetailKey.SHIELD_AGENT_PORT)}"
+        "${kubernetesServiceConfig.getKubernetesRedisHost()}:${ServiceDetailsHelper.from(serviceInstance.details).getValue(ShieldServiceDetailKey.SHIELD_AGENT_PORT)}"
     }
 }
