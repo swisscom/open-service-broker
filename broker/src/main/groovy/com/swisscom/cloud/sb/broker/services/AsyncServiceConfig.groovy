788b9ac7bda50e4c79dc57172a3a841e8316a0b0
Murat Celep <murat.celep@swisscom.com>
2017-10-17 14:22:50 +0200

    add comment

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/AsyncServiceConfig.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/AsyncServiceConfig.groovy
@@ -1,10 +1,10 @@
 package com.swisscom.cloud.sb.broker.services
 
 import com.swisscom.cloud.sb.broker.cfextensions.endpoint.EndpointConfig
-import com.swisscom.cloud.sb.broker.services.bosh.BoshConfig
 import groovy.transform.CompileStatic
 
 @CompileStatic
+//TODO change this to inherit from Config and *NOT* EndpointConfig
 trait AsyncServiceConfig implements EndpointConfig {
     int retryIntervalInSeconds
     int maxRetryDurationInMinutes
