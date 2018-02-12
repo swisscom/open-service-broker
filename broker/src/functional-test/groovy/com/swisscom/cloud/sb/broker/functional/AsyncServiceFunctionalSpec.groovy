d166e19b8a4e145c8b537d9facde4be6cc0c6594
Philipp Meier <philipp.meier2@swisscom.com>
2018-02-12 10:18:09 +0100

    Add func provisioning tests for Cloud Foundry and Kubernetes Context

--- broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/AsyncServiceFunctionalSpec.groovy
+++ broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/AsyncServiceFunctionalSpec.groovy
@@ -1,10 +1,21 @@
 package com.swisscom.cloud.sb.broker.functional
 
+import com.swisscom.cloud.sb.broker.context.ContextPersistenceService
+import com.swisscom.cloud.sb.broker.model.repository.ContextRepository
+import com.swisscom.cloud.sb.broker.model.repository.ServiceInstanceRepository
 import com.swisscom.cloud.sb.broker.services.common.ServiceProviderLookup
 import com.swisscom.cloud.sb.broker.util.test.DummyServiceProvider
 import com.swisscom.cloud.sb.client.model.LastOperationState
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.servicebroker.model.CloudFoundryContext
+import org.springframework.cloud.servicebroker.model.KubernetesContext
 
 class AsyncServiceFunctionalSpec extends BaseFunctionalSpec {
+    @Autowired
+    private ServiceInstanceRepository serviceInstanceRepository
+    @Autowired
+    private ContextRepository contextRepository
+
     private int processDelayInSeconds = DummyServiceProvider.RETRY_INTERVAL_IN_SECONDS * 2
 
     def setup() {
@@ -15,11 +26,17 @@ class AsyncServiceFunctionalSpec extends BaseFunctionalSpec {
         serviceLifeCycler.cleanup()
     }
 
-    def "provision async service instance"() {
+    def "provision async service instance without context"() {
+        given:
+        def serviceInstanceGuid = UUID.randomUUID().toString()
+        serviceLifeCycler.setServiceInstanceId(serviceInstanceGuid)
+
         when:
         serviceLifeCycler.createServiceInstanceAndAssert(DummyServiceProvider.RETRY_INTERVAL_IN_SECONDS * 4, true, true, ['delay': String.valueOf(processDelayInSeconds)])
+
         then:
         serviceLifeCycler.getServiceInstanceStatus().state == LastOperationState.SUCCEEDED
+        assertCloudFoundryContext(serviceInstanceGuid)
     }
 
     def "deprovision async service instance"() {
@@ -27,5 +44,56 @@ class AsyncServiceFunctionalSpec extends BaseFunctionalSpec {
         serviceLifeCycler.deleteServiceInstanceAndAssert(true, DummyServiceProvider.RETRY_INTERVAL_IN_SECONDS * 4)
         then:
         serviceLifeCycler.getServiceInstanceStatus().state == LastOperationState.SUCCEEDED
+    }
+
+    def "provision async service instance with CloudFoundryContext"() {
+        given:
+        def serviceInstanceGuid = UUID.randomUUID().toString()
+        serviceLifeCycler.setServiceInstanceId(serviceInstanceGuid)
+        def context = new CloudFoundryContext("org_id", "space_id")
+
+        when:
+        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false, null, context)
+
+        then:
+        assertCloudFoundryContext(serviceInstanceGuid)
+        noExceptionThrown()
+
+        cleanup:
+        serviceLifeCycler.deleteServiceInstanceAndAssert(true, DummyServiceProvider.RETRY_INTERVAL_IN_SECONDS * 4)
+    }
+
+    def "provision async service instance with KubernetesContext"() {
+        given:
+        def serviceInstanceGuid = UUID.randomUUID().toString()
+        serviceLifeCycler.setServiceInstanceId(serviceInstanceGuid)
+        def context = new KubernetesContext("namespace_guid")
+
+        when:
+        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false, null, context)
+
+        then:
+        assertKubernetesContext(serviceInstanceGuid)
+        noExceptionThrown()
+
+        cleanup:
+        serviceLifeCycler.deleteServiceInstanceAndAssert(true, DummyServiceProvider.RETRY_INTERVAL_IN_SECONDS * 4)
+    }
+
+    void assertCloudFoundryContext(String serviceInstanceGuid) {
+        def serviceInstance = serviceInstanceRepository.findByGuid(serviceInstanceGuid)
+        assert serviceInstance != null
+
+        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.PLATFORM, serviceInstance) != null
+        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.CF_ORGANIZATION_GUID, serviceInstance) != null
+        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.CF_SPACE_GUID, serviceInstance) != null
+    }
+
+    void assertKubernetesContext(String serviceInstanceGuid) {
+        def serviceInstance = serviceInstanceRepository.findByGuid(serviceInstanceGuid)
+        assert serviceInstance != null
+
+        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.PLATFORM, serviceInstance) != null
+        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.KUBENETES_NAMESPACE, serviceInstance) != null
     }
 }
--- broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/BindingParametersFunctionalSpec.groovy
+++ broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/BindingParametersFunctionalSpec.groovy
@@ -1,19 +1,9 @@
 package com.swisscom.cloud.sb.broker.functional
 
-import com.swisscom.cloud.sb.broker.context.ContextPersistenceService
-import com.swisscom.cloud.sb.broker.model.repository.ContextRepository
-import com.swisscom.cloud.sb.broker.model.repository.ServiceInstanceRepository
 import com.swisscom.cloud.sb.broker.services.common.ServiceProviderLookup
 import com.swisscom.cloud.sb.broker.util.test.DummySynchronousServiceProvider
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.servicebroker.model.CloudFoundryContext
 
 class BindingParametersFunctionalSpec extends BaseFunctionalSpec {
-
-    @Autowired
-    private ContextRepository contextRepository
-    @Autowired
-    private ServiceInstanceRepository serviceInstanceRepository
 
     def setup() {
         serviceLifeCycler.createServiceIfDoesNotExist('SyncDummy', ServiceProviderLookup.findInternalName(DummySynchronousServiceProvider.class))
@@ -23,46 +13,14 @@ class BindingParametersFunctionalSpec extends BaseFunctionalSpec {
         serviceLifeCycler.cleanup()
     }
 
-    def "provision async service instance"() {
+    def "provision async service instance and bind with parameters"() {
         given:
-        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false, null)
+        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false)
 
         when:
         serviceLifeCycler.bindServiceInstanceAndAssert(null, ['key1': 'value1'])
 
         then:
-        noExceptionThrown()
-    }
-
-    def "provision async service instance with Context"() {
-        given:
-        def serviceInstanceGuid = UUID.randomUUID().toString()
-        serviceLifeCycler.setServiceInstanceId(serviceInstanceGuid)
-        serviceLifeCycler.setServiceBindingId(UUID.randomUUID().toString())
-        def context = new CloudFoundryContext("org_id", "space_id")
-        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false, null, context)
-
-        when:
-        serviceLifeCycler.bindServiceInstanceAndAssert(null, ['key1': 'value1'])
-
-        then:
-        assertCloudFoundryContext(serviceInstanceGuid)
-        noExceptionThrown()
-    }
-
-    def "provision async service instance and Binding with Context"() {
-        given:
-        def serviceInstanceGuid = UUID.randomUUID().toString()
-        serviceLifeCycler.setServiceInstanceId(serviceInstanceGuid)
-        serviceLifeCycler.setServiceBindingId(UUID.randomUUID().toString())
-        serviceLifeCycler.createServiceInstanceAndAssert(0, false, false, null)
-
-        when:
-        def context = new CloudFoundryContext("org_id", "space_id")
-        serviceLifeCycler.bindServiceInstanceAndAssert(null, ['key1': 'value1'], true, context)
-
-        then:
-        assertCloudFoundryContext(serviceInstanceGuid)
         noExceptionThrown()
     }
 
@@ -73,14 +31,4 @@ class BindingParametersFunctionalSpec extends BaseFunctionalSpec {
         then:
         noExceptionThrown()
     }
-
-    void assertCloudFoundryContext(String serviceInstanceGuid) {
-        def serviceInstance = serviceInstanceRepository.findByGuid(serviceInstanceGuid)
-        assert serviceInstance != null
-
-        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.PLATFORM, serviceInstance) != null
-        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.CF_ORGANIZATION_GUID, serviceInstance) != null
-        assert contextRepository.findByKeyAndServiceInstance(ContextPersistenceService.CF_SPACE_GUID, serviceInstance) != null
-    }
-
 }
