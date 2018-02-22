062d1ca6e1fa44409600a2e4d7af74e03974c28a
Christoph Seiler <christoph.seiler@swisscom.com>
2018-02-22 15:06:06 +0100

    Update ApplicationUserInitializer to disable/enable Users based on the application configuration.
    Empower the update to also update passwords and role for already exisiting users if it changed in the application configuration.

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/security/ApplicationUserInitializer.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/security/ApplicationUserInitializer.groovy
@@ -1,6 +1,7 @@
 package com.swisscom.cloud.sb.broker.security
 
 import com.swisscom.cloud.sb.broker.config.ApplicationUserConfig
+import com.swisscom.cloud.sb.broker.config.UserConfig
 import com.swisscom.cloud.sb.broker.model.ApplicationUser
 import com.swisscom.cloud.sb.broker.model.repository.ApplicationUserRepository
 import groovy.util.logging.Slf4j
@@ -25,38 +26,72 @@ class ApplicationUserInitializer {
 
     @PostConstruct
     void init() throws Exception {
-        checkForMissingUsers()
-        addApplicationUsers()
+        synchronizeApplicationUsers()
     }
 
-    void checkForMissingUsers() {
-        List<ApplicationUser> users = userRepository.findAll()
-        def dbUsernames = users.collect { it.username }
-
-        def configUsernames = applicationUserConfig.platformUsers.collect { it.users.username }.flatten()
-        if (configUsernames.size() != 0) {
-            if (!configUsernames.containsAll(dbUsernames)) {
-                throw new RuntimeException("Missing application user configuration exception. DB Username list - ${dbUsernames}")
-            }
+    void disableApplicationUser(ApplicationUser user) {
+        if (!user.enabled)
+        {
+            user.enabled = false;
+            userRepository.saveAndFlush(user);
         }
     }
 
-    void addApplicationUsers() {
+    void synchronizeApplicationUsers() {
+        def allDbUser = userRepository.findAll();
+
         applicationUserConfig.platformUsers.each {
-            g ->
-                g.users.each {
-                    u ->
-                    def user = userRepository.findByUsername(u.username)
-                    if (!user) {
-                        user = new ApplicationUser()
-                        user.username = u.username
-                        user.password = passwordEncoder.encode(u.password)
-                        user.enabled = true
-                        user.role = u.role
-                        user.platformGuid = g.guid
-                        userRepository.saveAndFlush(user)
-                    }
+            platformGroup ->
+                platformGroup.users.each {
+                    configUser ->
+                        def dbUser = allDbUser.findOne { user -> user.username == configUser.username && user.platformGuid == platformGroup.guid }
+                        if (dbUser == null)
+                        {
+                            addApplicationUser(platformGroup.guid, configUser)
+                        }
+                        else
+                        {
+                            synchronizeApplicationUser(dbUser, configUser)
+                            allDbUser.remove(dbUser);
+                        }
             }
         }
+
+        allDbUser.each { oldUser -> disableApplicationUser(oldUser) };
+    }
+
+    void synchronizeApplicationUser(ApplicationUser user, UserConfig userConfig) {
+        def changed = false
+
+        if (user.password != passwordEncoder.encode(userConfig.password)) {
+            user.password = passwordEncoder.encode(userConfig.password)
+            changed = true;
+        }
+
+        if (user.role != userConfig.role) {
+            user.role = userConfig.role
+            changed = true;
+        }
+
+        if (!user.enabled)
+        {
+            user.enabled = true;
+            changed = true;
+        }
+
+        if (changed)
+        {
+            userRepository.saveAndFlush(user);
+        }
+    }
+
+    void addApplicationUser(String platformGuid, UserConfig userConfig) {
+        user = new ApplicationUser()
+        user.username = userConfig.username
+        user.password = passwordEncoder.encode(userConfig.password)
+        user.enabled = true
+        user.role = userConfig.role
+        user.platformGuid = platformGuid
+        userRepository.saveAndFlush(user)
     }
 }
