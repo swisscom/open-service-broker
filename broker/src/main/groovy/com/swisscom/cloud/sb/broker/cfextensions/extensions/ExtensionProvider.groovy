657e67f31e1aeb644c200122746108df48b41d67
Zohaib Khan <zohaib.khan@swisscom.com>
2018-03-07 15:07:46 -0800

    Add get openapi doc endpoint

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/extensions/ExtensionProvider.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/extensions/ExtensionProvider.groovy
@@ -6,6 +6,16 @@ import com.swisscom.cloud.sb.broker.backup.shield.dto.JobStatus
 import com.swisscom.cloud.sb.broker.backup.shield.dto.TaskDto
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.servicebroker.model.Catalog
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+import sun.awt.AppContext
 
 @Slf4j
 trait ExtensionProvider{
@@ -33,5 +43,25 @@ trait ExtensionProvider{
 
     def queueExtension(JobConfig jobConfig){
         jobManager.queue(jobConfig)
+    }
+
+    String getApi(){
+        RestTemplate restTemplate = new RestTemplate()
+
+        String swaggerJson = restTemplate.getForEntity("http://localhost:8080/v2/api-docs", String.class).body
+
+        log.info("swaggerJson = " + swaggerJson)
+        HttpHeaders headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED)
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>()
+        map.add("source", swaggerJson)
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers)
+
+        ResponseEntity<String> response = restTemplate.postForEntity( "https://mermade.org.uk/openapi-converter/api/v1/convert", request , String.class )
+
+        log.info("response = " + response)
+        return response.body
     }
 }
--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/controller/CFExtensionsController.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/controller/CFExtensionsController.groovy
@@ -3,10 +3,15 @@ package com.swisscom.cloud.sb.broker.controller
 import com.fasterxml.jackson.databind.util.ISO8601DateFormat
 import com.google.common.base.Optional
 import com.swisscom.cloud.sb.broker.cfextensions.endpoint.EndpointService
+import com.swisscom.cloud.sb.broker.cfextensions.extensions.ExtensionProvider
 import com.swisscom.cloud.sb.broker.cfextensions.serviceusage.ServiceUsageLookup
+import com.swisscom.cloud.sb.broker.cfextensions.serviceusage.ServiceUsageProvider
 import com.swisscom.cloud.sb.broker.error.ErrorCode
+import com.swisscom.cloud.sb.broker.model.Plan
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
 import com.swisscom.cloud.sb.broker.model.repository.ServiceInstanceRepository
+import com.swisscom.cloud.sb.broker.services.common.ServiceProvider
+import com.swisscom.cloud.sb.broker.services.common.ServiceProviderLookup
 import com.swisscom.cloud.sb.model.endpoint.Endpoint
 import com.swisscom.cloud.sb.model.usage.ServiceUsage
 import groovy.transform.CompileStatic
@@ -14,6 +19,7 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import org.yaml.snakeyaml.Yaml
 
 @CompileStatic
 @Api(value = "cf-extensions", description = "Additional operations for service instances")
@@ -29,6 +35,11 @@ class CFExtensionsController extends BaseController {
 
     @Autowired
     private ServiceInstanceRepository serviceInstanceRepository
+
+    @Autowired
+    private ServiceProviderLookup serviceProviderLookup
+
+    private ExtensionProvider extensionProvider
 
     @ApiOperation(value = "Get service instance usage", response = ServiceUsage.class)
     @RequestMapping(value = ['/v2/cf-ext/{service_instance}/usage', //deprecated, prefer the path below
@@ -59,6 +70,28 @@ class CFExtensionsController extends BaseController {
                     method = RequestMethod.GET)
     def endpoint(@PathVariable('service_instance') String serviceInstanceId) {
         endpointLookup.lookup(getAndCheckServiceInstance(serviceInstanceId))
+    }
+
+    @ApiOperation(value = "Get extension information", response = Yaml.class,
+            notes = "provides openapi 3.0 yaml")
+    @RequestMapping(value = ['/custom/service_instances/{service_instance}/api-docs'],
+            method = RequestMethod.GET)
+    def getApi(@PathVariable('service_instance') String serviceInstanceId) {
+//        ServiceInstance serviceInstance = serviceInstanceRepository.findByGuid(serviceInstanceId)
+//        if (serviceInstance) {
+//            ServiceProvider serviceProvider = serviceProviderLookup.findServiceProvider(serviceInstance.plan)
+//            if (!(serviceProvider instanceof ExtensionProvider)) {
+//                throw new RuntimeException("Service provider: ${serviceProvider.class.name} does not provide extension information")
+//            }
+//            ExtensionProvider exProvider = serviceProvider as ExtensionProvider
+//
+//            return exProvider.getApi()
+//        } else {
+//            return extensionProvider.getApi()
+//        }
+
+        ExtensionProvider serviceProvider = serviceProviderLookup.findServiceProvider(new Plan(serviceProviderClass: "dummyExtensionsServiceProvider")) as ExtensionProvider
+        return serviceProvider.getApi()
     }
 
 }
