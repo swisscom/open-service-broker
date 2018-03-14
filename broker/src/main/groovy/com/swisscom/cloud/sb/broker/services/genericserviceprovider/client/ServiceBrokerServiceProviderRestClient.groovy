08a21163a90b9eada846e24200d16ffd646a38aa
Melie <melanie.calame@swisscom.com>
2018-03-14 17:47:05 +0100

    implementation of the ServiceBrokerServiceProvider Facade, Config and RestClient to enable polling for async provisioning and deprovisioning

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/client/ServiceBrokerServiceProviderRestClient.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/client/ServiceBrokerServiceProviderRestClient.groovy
@@ -1,4 +1,40 @@
 package com.swisscom.cloud.sb.broker.services.genericserviceprovider.client
 
-class ServiceBrokerServiceClientRestClient {
+import com.swisscom.cloud.sb.broker.config.ApplicationUserConfig
+import com.swisscom.cloud.sb.broker.config.UserConfig
+import com.swisscom.cloud.sb.broker.services.genericserviceprovider.config.ServiceBrokerServiceProviderConfig
+import com.swisscom.cloud.sb.broker.util.RestTemplateBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestTemplate
+
+class ServiceBrokerServiceProviderRestClient {
+
+    private UserConfig cfAdminUser
+    private UserConfig cfExtUser
+    // maybe put url into sbspConfig?
+    private ServiceBrokerServiceProviderConfig sbspConfig
+    private RestTemplateBuilder restTemplateBuilder
+
+    @Autowired
+    EceRestClient(UserConfig cfAdminUser, UserConfig cfExtUser, ServiceBrokerServiceProviderConfig sbspConfig, RestTemplateBuilder restTemplateBuilder) {
+        this.cfAdminUser = cfAdminUser
+        this.cfExtUser = cfExtUser
+        this.sbspConfig = sbspConfig
+        this.restTemplateBuilder = restTemplateBuilder
+    }
+
+    HttpStatus getServiceInstanceInformation(String serviceInstanceId) {
+        def response = createExternalRestTemplate().exchange("http://localhost:8080/v2/cf-ext/${serviceInstanceId}/endpoint", HttpMethod.GET, null, String.class)
+        return response.statusCode == HttpStatus.ACCEPTED
+    }
+
+    private RestTemplate createAdminRestTemplate() {
+        restTemplateBuilder.withBasicAuthentication(cfAdminUser.username, cfAdminUser.password).build()
+    }
+
+    private RestTemplate createExternalRestTemplate() {
+        restTemplateBuilder.withBasicAuthentication(cfExtUser.username, cfExtUser.password).build()
+    }
 }
