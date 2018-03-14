b89af1c444e6752685527d00af7e8a79503c76db
Melie <melanie.calame@swisscom.com>
2018-03-14 14:24:40 +0100

    set up state machine for ServiceBrokerServiceProvider

---
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/statemachine/ServiceBrokerServiceProviderStateMachineContext.groovy
@@ -0,0 +1,8 @@
+package com.swisscom.cloud.sb.broker.services.genericserviceprovider.statemachine
+
+import com.swisscom.cloud.sb.broker.provisioning.statemachine.StateMachineContext
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ServiceBrokerServiceProviderStateMachineContext extends StateMachineContext{
+}
