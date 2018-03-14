2e43ac12124ef3f9de998b0343d14a84cdfb62f9
Melie <melanie.calame@swisscom.com>
2018-03-14 17:48:00 +0100

    extended the ServiceBrokerServiceProvider StateMachineContext and the ProvisionState

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/statemachine/ServiceBrokerServiceProviderStateMachineContext.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/statemachine/ServiceBrokerServiceProviderStateMachineContext.groovy
@@ -1,7 +1,6 @@
 package com.swisscom.cloud.sb.broker.services.genericserviceprovider.statemachine
 
 import com.swisscom.cloud.sb.broker.provisioning.statemachine.StateMachineContext
-
 import com.swisscom.cloud.sb.broker.services.genericserviceprovider.client.ServiceBrokerServiceProviderFacade
 import com.swisscom.cloud.sb.broker.services.genericserviceprovider.client.ServiceBrokerServiceProviderRestClient
 import groovy.transform.CompileStatic
