71ee2f2964fe4ce5f4e2342d216b1bc2b9da997a
Melie <melanie.calame@swisscom.com>
2018-03-14 17:47:05 +0100

    implementation of the ServiceBrokerServiceProvider Facade, Config and RestClient to enable polling for async provisioning and deprovisioning

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/client/ServiceBrokerServiceProviderFacade.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/genericserviceprovider/client/ServiceBrokerServiceProviderFacade.groovy
@@ -1,4 +1,23 @@
 package com.swisscom.cloud.sb.broker.services.genericserviceprovider.client
 
+import com.swisscom.cloud.sb.broker.model.ServiceInstance
+import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+
+import static com.swisscom.cloud.sb.broker.util.servicedetail.ServiceDetailsHelper.from
+
 class ServiceBrokerServiceProviderFacade {
+
+    private final ServiceBrokerServiceProviderRestClient sbspRestClient
+
+    @Autowired
+    ServiceBrokerServiceProviderFacade(ServiceBrokerServiceProviderRestClient sbspRestClient) {
+        this.sbspRestClient = sbspRestClient
+    }
+
+    boolean checkServiceProvisioningDone(String serviceInstanceId) {
+        return sbspRestClient.getServiceInstanceInformation(serviceInstanceId)
+
+    }
 }
