ade04457053cf951a70acb5fc51cc7347749c711
Zohaib Khan <zohaib.khan@swisscom.com>
2018-03-19 10:49:09 +0100

    Makes findUsage an extension

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/extensions/ExtensionConfig.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/extensions/ExtensionConfig.groovy
@@ -4,4 +4,7 @@ import com.swisscom.cloud.sb.broker.config.Config
 
 trait ExtensionConfig implements Config{
     String discoveryURL
+    String serverURL
+    Map<String, Map<String, String>> credentials
+    String adheresTo
 }
--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/serviceusage/ServiceUsageProvider.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfextensions/serviceusage/ServiceUsageProvider.groovy
@@ -1,9 +1,10 @@
 package com.swisscom.cloud.sb.broker.cfextensions.serviceusage
 
 import com.google.common.base.Optional
+import com.swisscom.cloud.sb.broker.cfextensions.extensions.ExtensionProvider
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
 import com.swisscom.cloud.sb.model.usage.ServiceUsage
 
-interface ServiceUsageProvider {
+interface ServiceUsageProvider extends ExtensionProvider{
     ServiceUsage findUsage(ServiceInstance serviceInstance, Optional<Date> enddate)
 }
