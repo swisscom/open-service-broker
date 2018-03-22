110746bae2d9daa52727210c9c89449784819ced
Melie <melanie.calame@swisscom.com>
2018-03-22 12:52:19 +0100

    corrected spelling mistake in a method in the ServiceLifeCycler class. The refactoring had an effect on this file

--- broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/BindingParametersFunctionalSpec.groovy
+++ broker/src/functional-test/groovy/com/swisscom/cloud/sb/broker/functional/BindingParametersFunctionalSpec.groovy
@@ -117,7 +117,7 @@ class BindingParametersFunctionalSpec extends BaseFunctionalSpec {
 
     def "deprovision async service instance"() {
         when:
-        serviceLifeCycler.deleteServiceBindingAndServiceInstaceAndAssert()
+        serviceLifeCycler.deleteServiceBindingAndServiceInstanceAndAssert()
 
         then:
         noExceptionThrown()
