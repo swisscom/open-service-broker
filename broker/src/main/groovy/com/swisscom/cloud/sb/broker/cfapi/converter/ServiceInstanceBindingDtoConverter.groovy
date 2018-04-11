5f6c69d86c4a2b6b323291f1966a7fe382551a03
ampersand8 <ampersand8@gmail.com>
2018-04-11 11:22:39 +0200

    return parameters and avoid nesting credentials in json

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfapi/converter/ServiceInstanceBindingDtoConverter.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/cfapi/converter/ServiceInstanceBindingDtoConverter.groovy
@@ -3,20 +3,22 @@ package com.swisscom.cloud.sb.broker.cfapi.converter
 import com.swisscom.cloud.sb.broker.binding.ServiceInstanceBindingResponseDto
 import com.swisscom.cloud.sb.broker.converter.AbstractGenericConverter
 import com.swisscom.cloud.sb.broker.model.ServiceBinding
-import groovy.transform.CompileStatic
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import groovy.util.logging.Slf4j
 import org.springframework.stereotype.Component
 
-@CompileStatic
+@Slf4j
 @Component
 class ServiceInstanceBindingDtoConverter extends AbstractGenericConverter<ServiceBinding, ServiceInstanceBindingResponseDto> {
 
     @Override
     void convert(ServiceBinding source, ServiceInstanceBindingResponseDto prototype) {
-        prototype.credentials = source.credentials
-        prototype.parameters = null // TODO
+        Object credentials = new JsonSlurper().parseText(source.credentials).credentials
+        prototype.credentials = new JsonBuilder(credentials).toString()
+        prototype.parameters = source.parameters
         prototype.routeServiceUrl = null
         prototype.syslogDrainUrl = null
         prototype.volumeMounts = null
     }
-
 }
