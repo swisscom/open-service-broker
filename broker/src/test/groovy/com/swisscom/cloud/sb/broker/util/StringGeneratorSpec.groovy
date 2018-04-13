dd667153f1a1f4094b83c04c5bae2b65e2f7555e
ampersand8 <ampersand8@gmail.com>
2018-04-13 09:33:25 +0200

    make StringGenerator tests more readable

--- broker/src/test/groovy/com/swisscom/cloud/sb/broker/util/StringGeneratorSpec.groovy
+++ broker/src/test/groovy/com/swisscom/cloud/sb/broker/util/StringGeneratorSpec.groovy
@@ -7,35 +7,50 @@ import org.passay.EnglishCharacterData
 import spock.lang.Specification
 
 @Slf4j
-class StringGeneratorSpec  extends Specification {
+class StringGeneratorSpec extends Specification {
+
+    private final String upperCase = "A-Z"
+    private final String lowerCase = "a-z"
+    private final String numbers = "0-9"
+    private final String defaultSpecial = "‘~!@#\$%^&*()_\\-+={}\\[\\]\\\\\\\\/<>,.;?':| "
+    private final String customSpecial = "~!@"
+    private final String defaultDisallowed = upperCase + lowerCase + numbers + defaultSpecial
+    private final String customDisallowed = upperCase + lowerCase + numbers + customSpecial
+    private int length = 30
 
     def "Check string generation with default rules and length"() {
         when:
-        String password = StringGenerator.generateRandomStringWithRules()
-        log.info(password)
+        String numberOfCharacters = "(.{${length}})"
+        String complexityPattern = atLeast(upperCase, 2) + atLeast(lowerCase, 2) + atLeast(numbers, 2) + atLeast(defaultSpecial, 2) + numberOfCharacters
 
+        String password = StringGenerator.generateRandomStringWithRules()
         then:
         // check if password has default complexity
-        password =~ /(?=.*?[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\/<>,.;?':| ].*[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\/<>,.;?':| ])(?=.*?[a-z].*[a-z])(?=.*?[A-Z].*[A-Z])(?=.*?[0-9].*[0-9])(.{30})/
+        password =~ /${complexityPattern}/
         // check if password has any disallowed characters
-        password =~ /^[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\\/<>,.;?':| a-zA-Z0-9]*$/
+        password =~ /^[${defaultDisallowed}]*$/
     }
 
     def "Check string generation with default rules and length of 8"() {
         when:
-        String password = StringGenerator.generateRandomStringWithRules(8)
-        log.info(password)
+        length = 8
+        String numberOfCharacters = "(.{${length}})"
+        String complexityPattern = atLeast(upperCase, 2) + atLeast(lowerCase, 2) + atLeast(numbers, 2) + atLeast(defaultSpecial, 2) + numberOfCharacters
 
+        String password = StringGenerator.generateRandomStringWithRules(length)
         then:
         // check if password has default complexity
-        password =~ /(?=.*?[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\/<>,.;?':| ].*[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\/<>,.;?':| ])(?=.*?[a-z].*[a-z])(?=.*?[A-Z].*[A-Z])(?=.*?[0-9].*[0-9])(.{8})/
+        password =~ /${complexityPattern}/
         // check if password has any disallowed characters
-        password =~ /^[‘~!@#\u0024%^&*()_\-+={}\[\]\\\\\/<>,.;?':| a-zA-Z0-9]*$/
+        password =~ /^[${defaultDisallowed}]*$/
     }
 
     def "Check string generation with custom rules and default length"() {
         when:
         final int occurrences = 4
+        String numberOfCharacters = "(.{${length}})"
+        String complexityPattern = atLeast(upperCase, occurrences) + atLeast(lowerCase, occurrences) + atLeast(numbers, occurrences) + atLeast(customSpecial, occurrences) + numberOfCharacters
+
         final CharacterRule atLeastFourLowerCase = new CharacterRule(EnglishCharacterData.LowerCase, occurrences)
         final CharacterRule atLeastFourUpperCase = new CharacterRule(EnglishCharacterData.UpperCase, occurrences)
         final CharacterRule atLeastFourNumbers = new CharacterRule(EnglishCharacterData.Digit, occurrences)
@@ -47,18 +62,23 @@ class StringGeneratorSpec  extends Specification {
 
             @Override
             String getCharacters() {
-                return "~!@ "
+                return customSpecial
             }
         }, occurrences)
         String password = StringGenerator.generateRandomStringWithRules(30, [atLeastFourLowerCase, atLeastFourUpperCase, atLeastFourNumbers, atLeastFourSpecialCharacters])
-        log.info(password)
-        final String upperCase = "(?=.*?[A-Z].*[A-Z].*[A-Z].*[A-Z])"
-        final String lowerCase = "(?=.*?[a-z].*[a-z].*[a-z].*[a-z])"
-        final String numbers = "(?=.*?[0-9].*[0-9].*[0-9].*[0-9])"
-        final String specialCharacters = "(?=.*?[~!@ ].*[~!@ ].*[~!@ ].*[~!@ ])"
 
         then:
         // check if password has given complexity
-        password =~ /${upperCase}${lowerCase}${numbers}${specialCharacters}/
+        password =~ /${complexityPattern}/
+        // check if password has any disallowed characters
+        password =~ /^[${customDisallowed}]*$/
+    }
+
+    private String atLeast(String pattern, int repetitions) {
+        String result = "(?=.*?"
+        for (int i = 0; i < repetitions; i++) {
+            result += "[" + pattern + "].*"
+        }
+        result + ")"
     }
 }
