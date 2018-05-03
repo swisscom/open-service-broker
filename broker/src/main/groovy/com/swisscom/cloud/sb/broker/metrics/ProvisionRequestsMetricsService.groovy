6d6c8b89fd570c999c2db7c26a35646dd97a7743
Melie <melanie.calame@swisscom.com>
2018-05-03 17:42:57 +0200

    refactor ServiceBroker metrics exposition by generalising and moving the provisioning specific functions to the ServiceBrokerMetrics class

---
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionRequestsMetricsService.groovy
@@ -0,0 +1,53 @@
+package com.swisscom.cloud.sb.broker.metrics
+
+import com.swisscom.cloud.sb.broker.model.ServiceInstance
+import groovy.transform.CompileStatic
+import org.springframework.boot.actuate.metrics.Metric
+import org.springframework.stereotype.Service
+
+@Service
+@CompileStatic
+class ProvisionRequestsMetricsService extends ServiceBrokerMetrics {
+
+    private final String PROVISION_REQUEST = "provisionRequest"
+
+    @Override
+    boolean considerServiceInstance(ServiceInstance serviceInstance) {
+        // every provision request should be counted, whether the service instance has been deleted or not is irrelevant
+        return true;
+    }
+
+    @Override
+    String tag() {
+        return ProvisionRequestsMetricsService.class.getSimpleName()
+    }
+
+    @Override
+    Collection<Metric<?>> metrics() {
+        List<Metric<?>> metrics = new ArrayList<>()
+
+        retrieveTotalMetrics()
+        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${TOTAL}", total))
+        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${SUCCESS}", totalSuccess))
+        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${FAIL}", totalFailure))
+        metrics.add(new Metric<Double>("${PROVISION_REQUEST}.${SUCCESS}.${RATIO}", calculateRatio(total, totalSuccess)))
+        metrics.add(new Metric<Double>("${PROVISION_REQUEST}.${FAIL}.${RATIO}", calculateRatio(total, totalFailure)))
+
+        retrieveTotalMetricsPerService()
+        metrics = addCountersFromHashMapToMetrics(totalPerService, totalPerService, metrics, PROVISION_REQUEST, SERVICE, TOTAL)
+        metrics = addCountersFromHashMapToMetrics(totalPerService, totalSuccessPerService, metrics, PROVISION_REQUEST, SERVICE, SUCCESS)
+        metrics = addCountersFromHashMapToMetrics(totalPerService, totalFailurePerService, metrics, PROVISION_REQUEST, SERVICE, FAIL)
+
+        retrieveTotalMetricsPerPlan()
+        metrics = addCountersFromHashMapToMetrics(totalPerPlan, totalPerPlan, metrics, PROVISION_REQUEST, PLAN, TOTAL)
+        metrics = addCountersFromHashMapToMetrics(totalPerPlan, totalSuccessPerPlan, metrics, PROVISION_REQUEST, PLAN, SUCCESS)
+        metrics = addCountersFromHashMapToMetrics(totalPerPlan, totalFailurePerPlan, metrics, PROVISION_REQUEST, PLAN, FAIL)
+
+        /*def influx = new InfluxDBConnector().influxMetricsWriter()
+        for(Metric<?> m: metrics) {
+            influx.set(m)
+        }*/
+
+        return metrics
+    }
+}
