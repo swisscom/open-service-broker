29d9d5be5fe178f87eb90149473427f742b2dbcf
Melie <melanie.calame@swisscom.com>
2018-05-15 11:17:34 +0200

    implement method that retrieves the number of service bindings per service and exposes it

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
@@ -1,11 +1,14 @@
 package com.swisscom.cloud.sb.broker.metrics
 
+import com.swisscom.cloud.sb.broker.model.ServiceBinding
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
 import com.swisscom.cloud.sb.broker.model.repository.ServiceBindingRepository
+import com.swisscom.cloud.sb.broker.model.repository.ServiceInstanceRepository
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.actuate.metrics.Metric
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,17 +19,45 @@ class BindingMetricsService extends ServiceBrokerMetrics{
     @Autowired
     private ServiceBindingRepository serviceBindingRepository
 
+    @AutoConfigureOrder
+    private ServiceInstanceRepository serviceInstanceRepository
+
     private final String BINDING = "binding"
 
     private long totalNrOfBindings
+    private HashMap<String, Long> totalBindingsPerService
+
 
     void retrieveMetricsForTotalNrOfBindings() {
 
-        def allServiceBindings = serviceBindingRepository.findAll()
-        totalNrOfBindings = allServiceBindings.size()
+        def it = getServiceBindingIterator()
+        totalNrOfBindings = it.size()
 
         log.info("Total nr of provision requests: ${totalNrOfBindings}")
 
+    }
+
+    void retrieveTotalBindingsPerService(){
+        HashMap<String, Long> totalHm = new HashMap<>()
+
+        def it = getServiceBindingIterator()
+        while(it.hasNext()) {
+            def serviceBinding = it.next()
+            def serviceInstance = serviceBinding.serviceInstance
+            def service = serviceInstance.plan.service
+            def serviceName = "someService"
+            if(service) {
+                serviceName = service.name
+            }
+            totalHm = addEntryToHm(totalHm, serviceName)
+        }
+        totalBindingsPerService = totalHm
+        log.info("${tag()} total bindings per service: ${totalPerService}")
+    }
+
+    ListIterator<ServiceBinding> getServiceBindingIterator() {
+        def allServiceBindings = serviceBindingRepository.findAll()
+        return allServiceBindings.listIterator()
     }
 
     @Override
@@ -35,6 +66,9 @@ class BindingMetricsService extends ServiceBrokerMetrics{
 
         retrieveMetricsForTotalNrOfBindings()
         metrics.add(new Metric<Long>("${BINDING}.${TOTAL}.${TOTAL}", totalNrOfBindings))
+
+        retrieveTotalBindingsPerService()
+        metrics = addCountersFromHashMapToMetrics(totalBindingsPerService, totalBindingsPerService, metrics, BINDING, SERVICE, TOTAL)
 
         return metrics
     }
