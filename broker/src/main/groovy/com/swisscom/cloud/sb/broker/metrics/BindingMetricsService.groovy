14c8a3e734fe0cc5681e64008a89d023d72a078f
Melie <melanie.calame@swisscom.com>
2018-05-15 16:53:32 +0200

    MPSPHOENIX-30: Implement metrics collection of lifecycle time per service

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
@@ -14,13 +14,10 @@ import org.springframework.stereotype.Service
 @Service
 @CompileStatic
 @Slf4j
-class BindingMetricsService extends ServiceBrokerMetrics{
+class BindingMetricsService extends ServiceBrokerMetrics {
 
     @Autowired
     private ServiceBindingRepository serviceBindingRepository
-
-    @AutoConfigureOrder
-    private ServiceInstanceRepository serviceInstanceRepository
 
     private final String BINDING = "binding"
     private final String BINDING_REQUEST = "bindingRequest"
@@ -32,29 +29,23 @@ class BindingMetricsService extends ServiceBrokerMetrics{
     private HashMap<String, Long> totalSuccessfulBindingRequestsPerService = new HashMap<>()
     private HashMap<String, Long> totalFailedBindingRequestsPerService = new HashMap<>()
 
-
-
-
-
     void retrieveMetricsForTotalNrOfBindings() {
-
         def it = getServiceBindingIterator()
         totalSuccessfulNrOfBindings = it.size()
 
         log.info("Total nr of provision requests: ${totalSuccessfulNrOfBindings}")
-
     }
 
-    void retrieveTotalSuccessfulBindingsPerService(){
+    void retrieveTotalSuccessfulBindingsPerService() {
         HashMap<String, Long> totalHm = new HashMap<>()
 
         def it = getServiceBindingIterator()
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             def serviceBinding = it.next()
             def serviceInstance = serviceBinding.serviceInstance
             def service = serviceInstance.plan.service
             def serviceName = "someService"
-            if(service) {
+            if (service) {
                 serviceName = service.name
             }
             totalHm = addEntryToHm(totalHm, serviceName)
@@ -81,7 +72,7 @@ class BindingMetricsService extends ServiceBrokerMetrics{
 
     void calculateFailedBindingRequestsPerService() {
         def it = totalBindingRequestsPerService.iterator()
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             def next = (Map.Entry<String, Long>) it.next()
             def key = next.getKey()
             def totalValue = next.getValue()
@@ -89,15 +80,6 @@ class BindingMetricsService extends ServiceBrokerMetrics{
             def failureValue = totalValue - successValue
             totalFailedBindingRequestsPerService.put(key, failureValue)
         }
-    }
-
-    String getServiceName(ServiceInstance serviceInstance) {
-        def cfService = serviceInstance.plan.service
-        def cfServiceName = "someService"
-        if (cfService) {
-            cfServiceName = cfService.name
-        }
-        return cfServiceName
     }
 
     @Override
