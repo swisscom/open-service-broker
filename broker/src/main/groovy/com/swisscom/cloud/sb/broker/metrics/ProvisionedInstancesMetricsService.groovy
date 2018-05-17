0581f24a90f579f9ca422b2ae229884d576c9f30
Melie <melanie.calame@swisscom.com>
2018-05-17 14:50:04 +0200

    #97: Inject MeterRegistry, not implement adding of gauges to the meter registry

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionedInstancesMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionedInstancesMetricsService.groovy
@@ -59,6 +59,11 @@ class ProvisionedInstancesMetricsService extends ServiceBrokerMetrics {
     }
 
     @Override
+    void addMetricsToMeterRegistry(MeterRegistry meterRegistry) {
+
+    }
+
+    @Override
     Collection<Metric<?>> metrics() {
         List<Metric<?>> metrics = new ArrayList<>()
         List<ServiceInstance> serviceInstanceList = serviceInstanceRepository.findAll()
