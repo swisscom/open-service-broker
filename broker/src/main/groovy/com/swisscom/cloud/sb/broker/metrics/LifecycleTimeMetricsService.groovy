330123278278a6c0f1559e442f66b6477f3cc291
Melie <melanie.calame@swisscom.com>
2018-05-17 14:49:31 +0200

    #97: Inject MeterRegistry to export custom metrics to InfluxDB and implement method that adds gauges to the meter registry for the KPIs

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/LifecycleTimeMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/LifecycleTimeMetricsService.groovy
@@ -25,6 +25,8 @@ import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
+import java.time.Duration
+
 @Service
 @CompileStatic
 class LifecycleTimeMetricsService extends ServiceBrokerMetricsService {
