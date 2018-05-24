51eecad3a99250bc3097358248fe47e66d14926a
Melie <melanie.calame@swisscom.com>
2018-05-24 10:23:14 +0200

    Implement writing initial value to influxDB

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionedInstancesMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionedInstancesMetricsService.groovy
@@ -37,11 +37,6 @@ class ProvisionedInstancesMetricsService extends ServiceBrokerMetrics {
     }
 
     @Override
-    void addMetricsToMeterRegistry(MeterRegistry meterRegistry) {
-
-    }
-
-    @Override
     Collection<Metric<?>> metrics() {
         List<Metric<?>> metrics = new ArrayList<>()
         List<ServiceInstance> serviceInstanceList = serviceInstanceRepository.findAll()
