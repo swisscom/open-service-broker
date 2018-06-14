9b8ec4da08b3abca9a123fa3893f01340aa19aaf
Melie <melanie.calame@swisscom.com>
2018-06-14 07:33:43 +0200

    #97: Instantiate variables which otherwise would be null and crash the app

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/LifecycleTimeMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/LifecycleTimeMetricsService.groovy
@@ -25,8 +25,6 @@ import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
-import java.time.Duration
-
 @Service
 @CompileStatic
 class LifecycleTimeMetricsService extends ServiceBrokerMetricsService {
