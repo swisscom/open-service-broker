a2b24f2f3df2165e2fb16a150e8100d0e8004f71
Melie <melanie.calame@swisscom.com>
2018-06-19 12:49:23 +0200

    #97: Add integreation tests for exposing metrics

---
+++ broker/src/integration-test/groovy/com/swisscom/cloud/sb/broker/metrics/measurements/BindingTotalTotalPoint.groovy
@@ -0,0 +1,15 @@
+package com.swisscom.cloud.sb.broker.metrics.measurements
+
+import org.influxdb.annotation.Column
+import org.influxdb.annotation.Measurement
+
+import java.time.Instant
+
+@Measurement(name = "binding_total_total")
+class BindingTotalTotalPoint {
+
+    @Column(name = "time")
+    Instant time
+    @Column(name = "value")
+    double value
+}
