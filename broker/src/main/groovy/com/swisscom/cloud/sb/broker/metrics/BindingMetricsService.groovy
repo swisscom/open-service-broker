391e7129c74d41300cf59f863ed31ab123f1c340
Melie <melanie.calame@swisscom.com>
2018-06-19 12:55:19 +0200

    #97: Remove method that exposes metrics to /metrics endpoint since the metrics can be sent directly to InfluxDB

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/BindingMetricsService.groovy
@@ -100,8 +100,6 @@ class BindingMetricsService extends ServiceBrokerMetrics {
     }
 
     double getSuccessfulBindingCount(Entry<String, Long> entry) {
-        log.info("Entry: ${entry.getKey()}, ${entry.getValue()}")
-        log.info("ServiceBindingRepository size: ${serviceBindingRepository.findAll().size()}")
         def serviceBindings = serviceBindingRepository.findAll()
         if (serviceBindings.size() > 0) {
             return retrieveTotalNrOfSuccessfulBindingsPerService(serviceBindings).get(entry.getKey()).toDouble()
@@ -109,7 +107,7 @@ class BindingMetricsService extends ServiceBrokerMetrics {
         0.0
     }
 
-    double getBindingRequestCount(HashMap<String, Long> bindingRequestsPerService, Entry<String, Long> entry) {
+    double getBindingRequestCountForEntryFromHashMap(HashMap<String, Long> bindingRequestsPerService, Entry<String, Long> entry) {
         if (bindingRequestsPerService.size() > 0) {
             return bindingRequestsPerService.get(entry.getKey()).toDouble()
         }
@@ -132,7 +130,7 @@ class BindingMetricsService extends ServiceBrokerMetrics {
         }
         totalBindingRequestsPerService.each { entry ->
             addMetricsGauge(meterRegistry, "${BINDING_REQUEST}.${SERVICE}.${TOTAL}.${entry.getKey()}", {
-                getBindingRequestCount(totalBindingRequestsPerService, entry)
+                getBindingRequestCountForEntryFromHashMap(totalBindingRequestsPerService, entry)
             })
         }
 
@@ -141,7 +139,7 @@ class BindingMetricsService extends ServiceBrokerMetrics {
         }
         totalSuccessfulBindingRequestsPerService.each { entry ->
             addMetricsGauge(meterRegistry, "${BINDING_REQUEST}.${SERVICE}.${SUCCESS}.${entry.getKey()}", {
-                getBindingRequestCount(totalSuccessfulBindingRequestsPerService, entry)
+                getBindingRequestCountForEntryFromHashMap(totalSuccessfulBindingRequestsPerService, entry)
             })
         }
 
@@ -150,26 +148,9 @@ class BindingMetricsService extends ServiceBrokerMetrics {
         }
         totalFailedBindingRequestsPerService.each { entry ->
             addMetricsGauge(meterRegistry, "${BINDING_REQUEST}.${SERVICE}.${FAIL}.${entry.getKey()}", {
-                getBindingRequestCount(totalFailedBindingRequestsPerService, entry)
+                getBindingRequestCountForEntryFromHashMap(totalFailedBindingRequestsPerService, entry)
             })
         }
-    }
-
-    @Override
-    Collection<Metric<?>> metrics() {
-        List<Metric<?>> metrics = new ArrayList<>()
-        List<ServiceBinding> serviceBindingList = serviceBindingRepository.findAll()
-
-        def totalNrOfSuccessfulBinding = retrieveMetricsForTotalNrOfSuccessfulBindings(serviceBindingList)
-        metrics.add(new Metric<Long>("${BINDING}.${TOTAL}.${TOTAL}", totalNrOfSuccessfulBinding))
-
-        def totalNrOfSuccessfulBindingsPerService = retrieveTotalNrOfSuccessfulBindingsPerService(serviceBindingList)
-        metrics = addCountersFromHashMapToMetrics(totalNrOfSuccessfulBindingsPerService, totalNrOfSuccessfulBindingsPerService, metrics, BINDING, SERVICE, SUCCESS)
-        metrics = addCountersFromHashMapToMetrics(totalBindingRequestsPerService, totalBindingRequestsPerService, metrics, BINDING_REQUEST, SERVICE, TOTAL)
-        metrics = addCountersFromHashMapToMetrics(totalBindingRequestsPerService, totalSuccessfulBindingRequestsPerService, metrics, BINDING_REQUEST, SERVICE, SUCCESS)
-        metrics = addCountersFromHashMapToMetrics(totalBindingRequestsPerService, totalFailedBindingRequestsPerService, metrics, BINDING_REQUEST, SERVICE, FAIL)
-
-        return metrics
     }
 
     @Override
--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionRequestsMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ProvisionRequestsMetricsService.groovy
@@ -32,29 +32,4 @@ class ProvisionRequestsMetricsService extends ServiceBrokerMetrics {
     String tag() {
         return ProvisionRequestsMetricsService.class.getSimpleName()
     }
-
-    @Override
-    Collection<Metric<?>> metrics() {
-        List<Metric<?>> metrics = new ArrayList<>()
-        List<ServiceInstance> serviceInstanceList = serviceInstanceRepository.findAll()
-
-        def totalMetrics = retrieveTotalMetrics(serviceInstanceList)
-        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${TOTAL}", totalMetrics.total))
-        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${SUCCESS}", totalMetrics.totalSuccess))
-        metrics.add(new Metric<Long>("${PROVISION_REQUEST}.${TOTAL}.${FAIL}", totalMetrics.totalFailures))
-        metrics.add(new Metric<Double>("${PROVISION_REQUEST}.${SUCCESS}.${RATIO}", calculateRatio(totalMetrics.total, totalMetrics.totalSuccess)))
-        metrics.add(new Metric<Double>("${PROVISION_REQUEST}.${FAIL}.${RATIO}", calculateRatio(totalMetrics.total, totalMetrics.totalFailures)))
-
-        def totalMetricsPerService = retrieveTotalMetricsPerService(serviceInstanceList)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerService.total, totalMetricsPerService.total, metrics, PROVISION_REQUEST, SERVICE, TOTAL)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerService.total, totalMetricsPerService.totalSuccess, metrics, PROVISION_REQUEST, SERVICE, SUCCESS)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerService.total, totalMetricsPerService.totalFailures, metrics, PROVISION_REQUEST, SERVICE, FAIL)
-
-        def totalMetricsPerPlan = retrieveTotalMetricsPerPlan(serviceInstanceList)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerPlan.total, totalMetricsPerPlan.total, metrics, PROVISION_REQUEST, PLAN, TOTAL)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerPlan.total, totalMetricsPerPlan.totalSuccess, metrics, PROVISION_REQUEST, PLAN, SUCCESS)
-        metrics = addCountersFromHashMapToMetrics(totalMetricsPerPlan.total, totalMetricsPerPlan.totalFailures, metrics, PROVISION_REQUEST, PLAN, FAIL)
-
-        return metrics
-    }
 }
