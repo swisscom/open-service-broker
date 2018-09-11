259ff499d870c3ff308776b3c456bf2e69fce317
Christoph Seiler <christoph.seiler@swisscom.com>
2018-09-11 07:34:57 +0200

    #153: Cleanup comments.

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/AsyncServiceProvider.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/AsyncServiceProvider.groovy
@@ -86,7 +86,7 @@ abstract class AsyncServiceProvider<T extends AsyncServiceConfig>
 
     @Override
     Collection<Endpoint> findEndpoints(ServiceInstance serviceInstance) {
-        // return endpointLookup.findEndpoints(serviceInstance, serviceConfig)
+        return endpointLookup.findEndpoints(serviceInstance, serviceConfig)
     }
 
 }
