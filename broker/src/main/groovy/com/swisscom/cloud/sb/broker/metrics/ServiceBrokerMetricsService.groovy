2dcee914394d0a00f47d0e2ee02c4d44fe5a471d
Calame Melanie, GHR-OSA-NEX-18 <Melanie.Calame@swisscom.com>
2018-09-26 13:25:54 +0200

    DEBUG: Add log at debug level to examine metrics being sent to influx

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ServiceBrokerMetricsService.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/metrics/ServiceBrokerMetricsService.groovy
@@ -16,6 +16,7 @@
 package com.swisscom.cloud.sb.broker.metrics
 
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
@@ -23,6 +24,7 @@ import io.micrometer.core.instrument.MeterRegistry
 import java.util.function.ToDoubleFunction
 
 @CompileStatic
+@Slf4j
 abstract class ServiceBrokerMetricsService {
 
     protected ServiceBrokerMetricsConfig serviceBrokerMetricsConfig
@@ -41,6 +43,7 @@ abstract class ServiceBrokerMetricsService {
         ToDoubleFunction<ServiceBrokerMetricsService> doubleFunction = new ToDoubleFunction<ServiceBrokerMetricsService>() {
             @Override
             double applyAsDouble(ServiceBrokerMetricsService serviceBrokerMetrics) {
+                log.debug("${name}: ${function()}")
                 function()
             }
         }
