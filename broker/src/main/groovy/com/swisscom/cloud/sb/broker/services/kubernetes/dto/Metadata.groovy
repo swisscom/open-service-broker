d29207699b6ccd4915d4819ced47af0db23fbed5
Adrian Kurt <adrian.kurt@swisscom.com>
2018-10-03 14:11:06 +0200

    fixes issue #175

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/dto/Metadata.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/dto/Metadata.groovy
@@ -18,10 +18,6 @@ package com.swisscom.cloud.sb.broker.services.kubernetes.dto
 import groovy.transform.ToString
 
 @ToString
-class ServiceResponse implements Serializable {
-    String kind
-    String apiVersion
-    Spec spec
-    Object status
-    Metadata metadata
+class Metadata implements Serializable {
+    String resourceVersion
 }
