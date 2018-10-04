631e4bc313a21f0224a56bad7182a0d2663e36ad
Adrian Kurt <adrian.kurt@swisscom.com>
2018-10-04 17:06:23 +0200

    add version watch to K8S update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
-
+- Add proper update versioning logic dor K8S update service
+- Fix update service for mongnodb-2
 
 
 ## [5.0.13] - 2018-10-04
---
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/services/kubernetes/dto/DeploymentList.groovy
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018 Swisscom (Switzerland) Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.swisscom.cloud.sb.broker.services.kubernetes.dto
+
+import groovy.transform.ToString
+
+@ToString
+class DeploymentList implements Serializable {
+    String kind
+    String apiVersion
+    List<Deployment> items
+    Object metadata
+}
+
+@ToString
+class Deployment implements Serializable {
+    Metadata metadata
+    Spec spec
+    Status status
+}
+
+@ToString
+class Status implements Serializable {
+    int observedGeneration
+    int replicas
+    int updatedReplicas
+    int readyReplicas
+    int availableReplicas
+    int unavailableReplicas
+    Object conditions
+}
