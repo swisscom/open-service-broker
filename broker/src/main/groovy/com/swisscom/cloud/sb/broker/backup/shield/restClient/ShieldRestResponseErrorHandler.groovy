7257e3e72790962833f26a1e12aafcb440867384
ampersand8 <ampersand8@gmail.com>
2018-11-06 16:22:49 +0100

    handle 501 and 404 from shield with ShieldResourceNotFoundException

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/backup/shield/restClient/ShieldRestResponseErrorHandler.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/backup/shield/restClient/ShieldRestResponseErrorHandler.groovy
@@ -27,7 +27,7 @@ class ShieldRestResponseErrorHandler implements ResponseErrorHandler {
     void handleError(ClientHttpResponse response) throws IOException{
         String errorMessage = "Rest call to Shield failed with status:${response.statusCode}"
         // Shield raises a 501 and not a 404 if you query GET /v1/task/null or /v1/task/; let's handle this as it were a 404
-        if (HttpStatus.NOT_IMPLEMENTED == response.statusCode) {
+        if (HttpStatus.NOT_IMPLEMENTED == response.statusCode || HttpStatus.NOT_FOUND == response.statusCode) {
             throw new ShieldResourceNotFoundException(errorMessage)
         } else {
             throw new ServiceBrokerException(errorMessage)
