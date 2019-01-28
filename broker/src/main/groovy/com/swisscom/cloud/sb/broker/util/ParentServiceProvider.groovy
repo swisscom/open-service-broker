2c3ed3341af818c8d4702310bd6270640d7d6bba
ampersand8 <ampersand8@gmail.com>
2019-01-28 15:00:02 +0100

    set default unlimited when max_children parameter is no set

--- broker/src/main/groovy/com/swisscom/cloud/sb/broker/util/ParentServiceProvider.groovy
+++ broker/src/main/groovy/com/swisscom/cloud/sb/broker/util/ParentServiceProvider.groovy
@@ -1,16 +1,21 @@
 package com.swisscom.cloud.sb.broker.util
 
+import com.swisscom.cloud.sb.broker.model.Parameter
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
 
 trait ParentServiceProvider {
     private static final String MAX_CHILDREN = "max_children"
 
     boolean hasActiveChildren(ServiceInstance serviceInstance) {
-        return serviceInstance.childs.any({!it.deleted})
+        return serviceInstance.childs.any({ !it.deleted })
     }
 
     boolean isFull(ServiceInstance serviceInstance) {
-        def param = serviceInstance.plan.parameters.find { it.name == MAX_CHILDREN }
-        return (param.value as int) >= serviceInstance.childs.count({!it.deleted})
+        Parameter param = serviceInstance.plan.parameters.find { it.name == MAX_CHILDREN }
+        if (param == null) {
+            return false
+        } else {
+            return (param.value as int) >= serviceInstance.childs.count({ !it.deleted })
+        }
     }
 }
