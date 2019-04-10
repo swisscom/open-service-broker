18dd77a919b4d696a5965cad6d0674bd1e8bd3b5
Christoph Seiler <christoph.seiler@swisscom.com>
2019-04-10 12:49:25 +0200

    Fix failed provision count as active children.

--- broker/core/src/main/groovy/com/swisscom/cloud/sb/broker/util/ParentServiceProvider.groovy
+++ broker/core/src/main/groovy/com/swisscom/cloud/sb/broker/util/ParentServiceProvider.groovy
@@ -1,13 +1,19 @@
 package com.swisscom.cloud.sb.broker.util
 
+import com.swisscom.cloud.sb.broker.model.LastOperation
 import com.swisscom.cloud.sb.broker.model.Parameter
 import com.swisscom.cloud.sb.broker.model.ServiceInstance
+import com.swisscom.cloud.sb.broker.model.repository.LastOperationRepository
+import org.springframework.beans.factory.annotation.Autowired
 
 trait ParentServiceProvider {
     private static final String MAX_CHILDREN = "max_children"
 
+    @Autowired(required = true)
+    LastOperationRepository lastOperationRepository
+
     boolean hasActiveChildren(ServiceInstance serviceInstance) {
-        return serviceInstance.childs.any({ !it.deleted })
+        return getActiveChildrenCount(serviceInstance) > 0
     }
 
     boolean isFull(ServiceInstance serviceInstance) {
@@ -15,7 +21,25 @@ trait ParentServiceProvider {
         if (param == null) {
             return false
         } else {
-            return (param.value as int) <= serviceInstance.childs.count({ !it.deleted })
+            return (param.value as int) <= getActiveChildrenCount(serviceInstance)
         }
+    }
+
+    int getActiveChildrenCount(ServiceInstance serviceInstance) {
+        List<ServiceInstance> undeletedChildren = serviceInstance.childs.findAll { si -> !si.deleted }
+        int completedUndeletedChildrenCount = undeletedChildren.count { si -> si.completed }
+
+        List<ServiceInstance> uncompletedChildren = undeletedChildren.findAll { si -> !si.completed }
+
+        int childrenProvisionInProgress = 0
+        uncompletedChildren.each {
+            si ->
+                def lastOperation = lastOperationRepository.findByGuid(si.guid)
+                if (lastOperation.status == LastOperation.Status.IN_PROGRESS) {
+                    childrenProvisionInProgress++
+                }
+        }
+
+        return completedUndeletedChildrenCount + childrenProvisionInProgress
     }
 }
