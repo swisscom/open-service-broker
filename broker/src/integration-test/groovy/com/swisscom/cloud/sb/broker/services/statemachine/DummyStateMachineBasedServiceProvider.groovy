426eb672d019a55cbfac86851edb7e5889c2ef44
ampersand8 <ampersand8@gmail.com>
2019-07-17 16:33:52 +0200

    bug: StateMachineBasedServiceProvider constructor missing
    Due to the inheritance used with StatemachineBasedServiceProvider and
    the changes made to AsyncServiceProvider, a corresponding constructor
    needs to be defined.

---
+++ broker/src/integration-test/groovy/com/swisscom/cloud/sb/broker/services/statemachine/DummyStateMachineBasedServiceProvider.groovy
@@ -0,0 +1,51 @@
+package com.swisscom.cloud.sb.broker.services.statemachine
+
+import com.swisscom.cloud.sb.broker.async.AsyncProvisioningService
+import com.swisscom.cloud.sb.broker.binding.BindRequest
+import com.swisscom.cloud.sb.broker.binding.BindResponse
+import com.swisscom.cloud.sb.broker.binding.UnbindRequest
+import com.swisscom.cloud.sb.broker.provisioning.ProvisioningPersistenceService
+import com.swisscom.cloud.sb.broker.provisioning.lastoperation.LastOperationJobContext
+import com.swisscom.cloud.sb.broker.provisioning.statemachine.StateMachine
+import com.swisscom.cloud.sb.broker.provisioning.statemachine.StateMachineBasedServiceProvider
+import com.swisscom.cloud.sb.broker.provisioning.statemachine.StateMachineContext
+import org.springframework.stereotype.Component
+
+@Component
+class DummyStateMachineBasedServiceProvider extends StateMachineBasedServiceProvider<DummyAsyncServiceConfig> {
+    DummyStateMachineBasedServiceProvider(AsyncProvisioningService asyncProvisioningService,
+                                          ProvisioningPersistenceService provisioningPersistenceService,
+                                          DummyAsyncServiceConfig serviceConfig) {
+        super(asyncProvisioningService, provisioningPersistenceService, serviceConfig)
+    }
+
+    @Override
+    protected StateMachine getProvisionStateMachine(LastOperationJobContext lastOperationJobContext) {
+        return null
+    }
+
+    @Override
+    protected StateMachine getDeprovisionStateMachine(LastOperationJobContext lastOperationJobContext) {
+        return null
+    }
+
+    @Override
+    protected StateMachine getUpdateStateMachine(LastOperationJobContext lastOperationJobContext) {
+        return null
+    }
+
+    @Override
+    protected StateMachineContext createStateMachineContext(LastOperationJobContext lastOperationJobContext) {
+        return null
+    }
+
+    @Override
+    BindResponse bind(BindRequest request) {
+        return null
+    }
+
+    @Override
+    void unbind(UnbindRequest request) {
+
+    }
+}
